9655ca0530985bc1ba901b67176319f305142f12
runafter <runafter82@gmail.com>
2017-01-07 02:37:12 +0900

    add navigation drawer

--- app/src/main/java/com/runafter/wtt/MainActivity.java
+++ app/src/main/java/com/runafter/wtt/MainActivity.java
@@ -2,19 +2,21 @@ package com.runafter.wtt;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.CursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -26,26 +28,36 @@ import io.realm.Realm;
 import io.realm.RealmBaseAdapter;
 import io.realm.RealmQuery;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView listLogs;
     private Realm realm;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         initDB();
-
-        setContentView(R.layout.activity_main);
         this.listLogs = (ListView)findViewById(R.id.logs);
         this.listLogs.setAdapter(logsListAdapter());
 
         startNotificationListenerService();
-
-        Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-        startActivity(intent);
     }
+
+
 
     private void startNotificationListenerService() {
         Intent intent = new Intent(this, MdmNotificationListenerService.class);
@@ -71,16 +83,16 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater.from(context).inflate(R.layout.listview_logs_item, parent, false);
 
-            ViewHolder viewHolder;
+            WorkingTimeLogAdapter.ViewHolder viewHolder;
             if (convertView == null) {
                 convertView = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.listview_logs_item, parent, false);
-                viewHolder = new ViewHolder();
+                viewHolder = new WorkingTimeLogAdapter.ViewHolder();
                 viewHolder.timeView = (TextView)convertView.findViewById(R.id.logs_time);
                 viewHolder.typeView = (TextView)convertView.findViewById(R.id.logs_type);
                 convertView.setTag(viewHolder);
             } else {
-                viewHolder = (ViewHolder) convertView.getTag();
+                viewHolder = (WorkingTimeLogAdapter.ViewHolder) convertView.getTag();
             }
 
             WorkingTimeLog item = adapterData.get(position);
@@ -99,9 +111,39 @@ public class MainActivity extends AppCompatActivity {
         return adapter;
     }
 
-    private WorkingTimeLog logOf(Date time, String type) {
-        return new WorkingTimeLog(time.getTime(), type);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+            Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
